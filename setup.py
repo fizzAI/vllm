13f491361f14071ef9032d8241d4a4740e9af525
fizzAI <fizzarolli@riseup.net>
2024-10-24 20:31:28 -0400

    force cuda backend on windows

--- setup.py
+++ setup.py
@@ -41,6 +41,8 @@ if not sys.platform.startswith("linux"):
         sys.platform)
     if sys.platform in "win32":
         logger.warning("Running on Windows. This is not officially supported, but vLLM may still work.")
+        logger.warning("Assuming CUDA backend will be used, others are not supported for Windows.")
+        VLLM_TARGET_DEVICE = "cuda"
     else:
         VLLM_TARGET_DEVICE = "empty"
 
